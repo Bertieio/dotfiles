83754919547026a17453e5bafee85e228449ce52
Bertie <b@bertie.io>
2024-05-27 20:24:40 +0100

    tmux.nix cleanup

--- user/tmux.nix
+++ user/tmux.nix
@@ -3,19 +3,6 @@
   pkgs-unstable,
   ...
 }:
-#let 
-  #rose-pine = pkgs.tmuxPlugins.mkTmuxPlugin
-  #  {
-  #    pluginName = "rose-pine";
-  #    version = "23.11";
-  #    src = pkgs.fetchFromGitHub {
-  #      owner = "rose-pine";
-  #      repo = "tmux";
-  #      rev = "23233037e48ea5f124b6186f8d232fda03326448";
-  #      sha256 = "sha256-0ccJVQIIOpHdr3xMIBC1wbgsARCNpmN+xMYVO6eu/SI=";
-  #    };
-  #  };
-  #in
   {
   programs.tmux = {
     enable = true;
