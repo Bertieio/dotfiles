a1ee0acc0e82b6e3be72eeb06cb3a5d4071a3a6e
Bertie <b@Bertie.io>
2024-06-02 14:31:56 +0100

    Added Drives

--- system/nas.nix
+++ system/nas.nix
@@ -12,4 +12,25 @@
     boot.supportedFilesystems = [ "zfs" ];
     boot.zfs.forceImportRoot = false;
     networking.hostId = "d6f3d1cf";
+
+    fileSystems."/Photos" = {
+        device = "tank1/Photos";
+        fsType = "zfs";
+        options = ["zfutil"];
+    };
+    fileSystems."/GameServers" = {
+        device = "tank1/GameServers";
+        fsType = "zfs";
+        options = ["zfutil"];
+    };
+    fileSystems."/Media" = {
+        device = "tank1/Media";
+        fsType = "zfs";
+        options = ["zfutil"];
+    };
+    fileSystems."/Files" = {
+        device = "tank1/Files";
+        fsType = "zfs";
+        options = ["zfutil"];
+    };
  }
