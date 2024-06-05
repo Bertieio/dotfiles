66dcddc8be835651c174f810b1101009bd077b84
B <B@bertie.io>
2024-06-05 21:00:06 +0100

    I cane read

--- system/hyprland.nix
+++ system/hyprland.nix
@@ -1,4 +1,4 @@
-{pkgs, pkgs-unstable, ...}:{
+{pkgs, pkgs-unstable, inputs, ...}:{
     programs.hyprland = {
         enable = true;
         package = inputs.hyprland.packages."${pkgs.system}".hyprland;
